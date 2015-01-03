83efe00d14a08f71bf7d4e9d915803fcfbd32f7b
Jon Staley <jmstaley@gmail.com>
2015-01-03 17:11:17 +0000

    add in files to track

---
+++ content/pages/about.md
@@ -0,0 +1,8 @@
+Title: About
+
+I'm a London based developer currently working for [mumoActive](http://mumoactive.com/).
+
+### Elsewhere:
+
+ * [Github](https://github.com/jmstaley)
+ * [Twitter](https://twitter.com/SpandexBob)
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jon'
+SITENAME = u'Jon Staley'
+SITEURL = 'jonstaley.co.uk'
+
+THEME='droidstrap'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+PROFILE_IMG_URL='/images/avatar.png'
+DISQUS_SITENAME='jonstaley.disqus.com'
